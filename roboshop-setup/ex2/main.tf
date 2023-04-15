453f351611b90769683fb29b17721963b7841daf
divyasree16 <kdivyasree50@gmail.com>
2023-04-15 06:49:01 +0100

    added a directory

--- roboshop-setup/ex2/main.tf
+++ roboshop-setup/ex2/main.tf
@@ -1,14 +1,12 @@
-data "aws.ami" "ami" {
+data "aws_ami" "ami" {
   most_recent = true
   name_regex  = "centos-8-DevOps-practice"
   owners      = [ "973714476881"]
 }
 
 resource "aws_instance" "frontend" {
-  ami                    = "ami-0089b8e98cd95257d"
+  count                  = 5
+  ami                    = data.aws_ami.ami.image_id
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0e978024c0690d6a4"]
-  tags = {
-    Name = "Frontend"
-  }
 }
