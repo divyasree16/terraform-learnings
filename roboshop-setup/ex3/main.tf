fb8ed8759fee4b04fccb5ccd314989de354c1f40
divyasree16 <kdivyasree50@gmail.com>
2023-04-22 00:32:43 +0100

    code dry

---
+++ roboshop-setup/ex3/main.tf
@@ -0,0 +1,29 @@
+data "aws_ami" "ami" {
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+  owners      = [ "973714476881"]
+}
+
+resource "aws_instance" "instances" {
+  for_each                  = var.instances
+  ami                    = data.aws_ami.ami.image_id
+  instance_type          = each.value["type"]
+  vpc_security_group_ids = ["sg-0e978024c0690d6a4"]
+}
+
+variable "instances" {
+  default = {
+    catalogue = {
+      name = "catalogue"
+      type = "t3.micro"
+    }
+    user = {
+      name = "user"
+      type = "t3.small"
+    }
+  }
+}
+
+output "ec2" {
+  value = aws_instance.instances ["catalogue"].public_ip
+}
