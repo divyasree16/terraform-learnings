08347097bf59a9cf1e8275585725d687410581d1
divyasree16 <kdivyasree50@gmail.com>
2023-06-08 01:45:14 +0100

    type change

--- s3-state/main.tf
+++ s3-state/main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 resource "aws_instance" "frontend" {
-  ami                    = "ami-0089b8e98cd95257d"
+  ami                    = "ami-03265a0778a880afb"
   instance_type          = "t3.micro"
   vpc_security_group_ids = ["sg-0e978024c0690d6a4"]
   tags = {
